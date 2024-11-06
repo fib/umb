3c56b4e58269d85b4b3b45877f849d2b6e8da16f
josh <0x0f0f@pm.me>
2024-11-05 22:15:21 -0500

    add sections, integrate with pocketbase

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# This script will attempt to scrape all classes from the catalog URL provided.
+# This script will attempt to scrape allcoursees from the catalog URL provided.
 # It is not guaranteed to generate correct data, but it should at least generate
 # a good starting point, which can then be corrected manually.
 
@@ -10,25 +10,28 @@ from enum import Enum
 from bs4 import BeautifulSoup
 import requests as r
 
+from pocketbase import PocketBase
+
 import json
 import pprint
 
 @dataclass
-class Class:
-    id: int
+class Course:
+    id: str
     subject: str
     number: str
     title: str
     credits: int
     attributes: list[str]
-    sections: dict
 
 @dataclass
 class Section:
-    course_id: int
+    course_id: str
     number: int
+    instructor: str
+    location: str
     days: str
-    times: list[str]
+    times: str
 
 distributions = {
     "arts": "AR",
@@ -47,8 +50,10 @@ course_listings_url = "https://www.umb.edu/course_catalog/courses/ugrd_{subject}
 course_info_url = "https://www.umb.edu/course_catalog/course_info/ugrd_{subject}_all_{number}"
 
 courses = []
+sections = []
 
-attributes = {}
+pb = PocketBase('http://127.0.0.1:8090')
+pb_admin = pb.admins.auth_with_password("jkotlerj@gmail.com", "!%yr#Nt6LBGbT4CL")
 
 subjects_page = r.get(subject_listings_url).text
 subjects = BeautifulSoup(subjects_page, 'html.parser').find('div', {'id': 'content'}).find_all('li')
@@ -63,15 +68,10 @@ for subject in subjects:
 
     subject_courses = BeautifulSoup(s, features="html.parser").find("ul", {"class": "showHideList"}).find_all("li")
 
-    # current subject being processed
-    subject_list = []
-
     for course in subject_courses:
         if h := course.find("h4"):
             course_number = h.text.split("\xa0\xa0")[0].rsplit(' ')[1]
             course_title = h.text.split("\xa0\xa0")[1].replace(" + ", "")
-
-            print(course_info_url.format(subject=subject, number=course_number))
 
             course_page = r.get(course_info_url.format(subject=subject, number=course_number)).text
             course_page = BeautifulSoup(course_page, features="html.parser")
@@ -96,56 +96,63 @@ for subject in subjects:
             except Exception:
                 course_attributes = []
 
-            # sections
-            try:
-                course_sections = {}
-                semesters = course_page.find_all("h3")[1:]
-                section_tables = course_page.find_all("table")
-                
-                for semester, semester_table in zip(semesters, section_tables):
-                    course_sections[semester.contents[0]] = []
-
-                    for section in semester_table.find_all("tr", {"class": "class-info-rows"}):
-                        section_number = section.find("td", {"data-label": "Section"}).contents[0]
-                        section_meeting = section.find("td", {"data-label": "Schedule/Time"}).contents
-
-                        section_days, section_time = None, None
-                        if len(section_meeting) == 3:
-                            section_days = section_meeting[0]
-                            section_time = section_meeting[2]
-
-                        course_sections[semester.contents[0]].append({
-                            "number": section_number,
-                            "days": section_days,
-                            "time": section_time
-                        })
-
-            except Exception:
-                course_sections = {}
-
-
             courses.append(
                 asdict(
-                    Class(
-                        course_id,
+                    Course(
+                        str(course_id).zfill(15),
                         subject,
                         course_number,
                         course_title,
                         course_credits,
                         course_attributes,
-                        course_sections
                     )
                 )
             )
 
+            print(courses[-1])
+            # pb.collection("courses").create(courses[-1])
+
+            # sections
+            semesters = course_page.find_all("h3")[1:]
+            section_tables = course_page.find_all("table")
+            
+            for semester, semester_table in zip(semesters, section_tables):
+
+                for section in semester_table.find_all("tr", {"class": "class-info-rows"}):
+                    section_number = section.find("td", {"data-label": "Section"}).contents[0]
+                    section_meeting = section.find("td", {"data-label": "Schedule/Time"}).contents
+                    section_instructor = " ".join(section.find("td", {"data-label": "Instructor"}).contents[0].strip().split(",")[::-1])
+                    section_location = section.find("td", {"data-label": "Location"}).contents
+
+                    section_location = section_location[0] if section_location != [] else None
+
+                    section_days, section_time = None, None
+                    if len(section_meeting) == 3:
+                        section_days = section_meeting[0]
+                        section_time = section_meeting[2]
+
+                    sections.append(
+                        asdict(
+                            Section(
+                                str(course_id).zfill(15),
+                                section_number,
+                                section_instructor,
+                                section_location,
+                                section_days,
+                                section_time,
+                            )
+                        )
+                    )
+
+                    print(sections[-1])
+                    # pb.collection("sections").create(sections[-1])
+
+
+
             course_id += 1
 
-            print(courses[-1])
 
     print(f"{subject} done")
 
 
 pprint.pp(courses)
-
-# with open("../course_catalog.json", "w") as f:
-#     json.dump({"subjects": subjects, "courses": courses}, f)
